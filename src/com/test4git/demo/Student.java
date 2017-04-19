1bc6263c3a9c85a142b44f2d5249328ac33250a6
方成 <fc1115646215@163.com>
2017-04-19 10:51:45 +0800

    添加学生姓名

--- src/com/test4git/demo/Student.java
+++ src/com/test4git/demo/Student.java
@@ -9,5 +9,16 @@ public class Student {
 		// TODO Auto-generated method stub
 
 	}
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 
 }
